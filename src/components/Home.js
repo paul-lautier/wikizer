a6f9085a0111c845b7183eefcc9f62816f5955e7
paul-lautier <paul.lautier@ynov.com>
2021-04-14 23:02:12 +0200

    shorten link Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,10 @@ function Home() {
       <section className="section featured">
         <div className="container">
           <div className="columns is-multiline is-vcentered">
-            <Link to="/search/népal" className="column is-half-desktop is-full-mobile is-half-tablet is-vcentered" id="art-one"><div>Népal</div> </Link>
-            <Link to="/search/SCH" className="column is-half-desktop is-full-mobile is-half-tablet" id="art-two"><div>SCH</div> </Link>
-            <Link to="/search/kekra" className="column is-half-desktop is-full-mobile is-half-tablet" id="art-three"><div>Kekra</div> </Link>
-            <Link to="/search/Damso" className="column is-half-desktop is-full-mobile is-half-tablet" id="art-four"><div>Damso</div> </Link>
+            <Link to="/search/népal"><div>Népal</div> </Link>
+            <Link to="/search/SCH"><div>SCH</div> </Link>
+            <Link to="/search/kekra"><div>Kekra</div> </Link>
+            <Link to="/search/Damso"><div>Damso</div> </Link>
           </div>
         </div>
       </section>
